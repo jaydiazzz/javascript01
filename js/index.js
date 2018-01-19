13068567d7d77915c8bd34bcaa0d13884541ebd8
Jay <jaydiazzz10@gmail.com>
2018-01-18 21:01:25 -0800

    just played with javascript during class

---
+++ js/index.js
@@ -0,0 +1,11 @@
+function worked(){
+  var con = console.log('hello world');
+}
+
+if(worked() == true){
+  console.log("hello world worked");
+}
+
+else if(worked() == false){
+  console.log("didn't work");
+}
